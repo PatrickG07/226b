19ec9598b39ed7308bfbafc3289a2313be636389
PatrickG07 <patrick.gartenmann@csbe.ch>
2016-12-19 17:21:52 +0100

    daily commit 1

---
+++ Calendar/src/src/ch/P1/Class/StartController.java
@@ -0,0 +1,17 @@
+package src.ch.P1.Class;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class StartController {
+	
+	@FXML
+	public void edit(ActionEvent event){
+		
+	}
+	
+	@FXML
+	public void create(ActionEvent event){
+		
+	}
+}
---
+++ Calendar/src/src/ch/P1/Class/ViewController.java
@@ -0,0 +1,12 @@
+package src.ch.P1.Class;
+
+import javafx.stage.Stage;
+
+/**
+ * This is something that makes me very proud of my programming skills. When a
+ * view controller class implements this interface, it will have the init method
+ * called as soon as the scene is ready.
+ */
+public interface ViewController {
+	abstract void init(Stage stage);
+}
